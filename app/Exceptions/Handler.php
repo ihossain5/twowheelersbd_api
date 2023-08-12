14e9a3a8c99014a77832ee76bf611e31945daf3e
ihossain5 <ismail.hossain5169@gmail.com>
2023-08-12 12:52:46 +0600

    exception handling added

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,15 @@
 
 namespace App\Exceptions;
 
+use BadMethodCallException;
+use Illuminate\Database\Eloquent\MassAssignmentException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Throwable;
+use Symfony\Component\HttpFoundation\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -30,11 +37,40 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $e)  
     {
+        if ($e instanceof NotFoundHttpException) {
+
+            return $this->errorResponse($e,'Route not found',Response::HTTP_NOT_FOUND);
+            
+        }elseif ($e instanceof UnauthorizedHttpException) {
+
+            return $this->errorResponse($e,'UNAUTHORIZED ',Response::HTTP_UNAUTHORIZED);
+
+        }elseif ($e instanceof MethodNotAllowedHttpException) {
+
+            return $this->errorResponse($e,'Method not allowed ',Response::HTTP_METHOD_NOT_ALLOWED);
+
+        }elseif ($e instanceof MassAssignmentException) {
+
+            return $this->errorResponse($e, '',500);
+
+        }elseif ($e instanceof ModelNotFoundException) {
+
+            return $this->errorResponse($e,'Data not found with ID '.$request->id);
+
+        } elseif ($e instanceof BadMethodCallException) {
+
+           return $this->errorResponse($e,'Method does not exist',Response::HTTP_BAD_REQUEST);
+        } 
+        else {
+           return $this->errorResponse($e);
+        }
+    }
+
+    private function errorResponse($e, $message = '',$code = 404){
         return response()->json([
             'status' => false,
-            'message' => $e->getMessage(),
-            'file' => $e->getFile(),
-            'line' => $e->getLine(),
-        ]);
+            'errors' => $e->getMessage(),
+            'message' => $message,
+        ],$code);
     }
 }
