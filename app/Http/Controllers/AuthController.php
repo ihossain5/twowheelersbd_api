0e9d4dd92cf67d7e97d23f6e13eabb79224f998b
ihossain5 <ismail.hossain5169@gmail.com>
2023-10-02 23:26:56 +0600

    bugs fixed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -165,16 +165,17 @@ class AuthController extends Controller
 
     public function updateProfie(Request $request){
         $this->validate($request,['name'=> 'required']);
-        
+
         $user = auth()->user();
         $image = $user->photo;
 
         if($request->image){
             if($image) (new ImageUoloadService())->deleteImage($image);
             $image = (new ImageUoloadService())->storeImage($request->image, 'user/',300,300);
+            $user->photo = $image;
         }
 
-        $user->photo = $image;
+     
         $user->name = $request->name;
         $user->save();
 
