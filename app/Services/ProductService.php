e177618e6770218349b174824f8ddc94f989db0f
ihossain5 <ismail.hossain5169@gmail.com>
2023-10-22 12:44:29 +0600

    bugs fixed

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -13,7 +13,7 @@ class ProductService {
     }
 
     public function select(){
-        return $this->product->query()->select('id', 'name','images', 'selling_price', 'average_rating','regular_price','discount','brand_id');
+        return $this->product->query()->select('id', 'name','images', 'selling_price', 'average_rating','regular_price','discount','brand_id','quantity');
     }
     // public function select(){
     //     return $this->product->query() ->select('id','sub_category_id', 'brand_id','shop_id', 'brand_model_id', 'additional_names', 'colors', 'description', 'video', 'sizes','catelogue_pdf', 'name','quantity','discount_type','discount','regular_price','discounted_price','is_available','images','status','sku','additional_name_1','additional_name_2','additional_name_3','additional_name_4','additional_name_5','average_rating');
