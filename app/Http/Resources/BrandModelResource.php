447bfd52ae4732adc25a6da174daec9a7a291856
ihossain5 <ismail.hossain5169@gmail.com>
2023-11-09 23:23:48 +0600

    brand api modified

--- app/Http/Resources/BrandModelResource.php
+++ app/Http/Resources/BrandModelResource.php
@@ -2,30 +2,42 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class BrandModelResource extends JsonResource
-{
+class BrandModelResource extends JsonResource {
     /**
      * Transform the resource into an array.
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
-    {
+    public function toArray(Request $request): array {
+        if ($request->routeIs('brands')) {
+            return [
+                'id'             => $this->id,
+                'name'           => $this->name,
+                'start_year'     => Carbon::parse($this->year_form)->format('Y-m-d'),
+                'end_year'       => Carbon::parse($this->year_to)->format('Y-m-d'),
+                'images'         => addUrl(collect(json_decode($this->images))->take(1)),
+                'total_products' => $this->products->count(),
+            ];
+        }
+
         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'description' => $this->info,
-            'images' =>  addUrl(collect(json_decode($this->images))),
-            'video' => $this->video,
-            'catelogue_pdf' => BASE_URL(). $this->catelogue_pdf,
-            'catelogues' => BrandModelCatelogueResource::collection($this->catelogues),
-            'colors' => BrandModelColorResource::collection($this->colors),
-            'specifications' => BrandModelSpicificationResource::collection($this->specifications),
-            'products' => $this->when($request->routeIs('model.products'), ProductResource::collection($this->products)),
+            'id'             => $this->id,
+            'name'           => $this->name,
+            'images'         => addUrl(collect(json_decode($this->images))),
             'total_products' => $this->products->count(),
+
+            'description'    => $this->info,
+            'video'          => $this->video,
+            'catelogue_pdf'  => BASE_URL() . $this->catelogue_pdf,
+            'catelogues'     => BrandModelCatelogueResource::collection($this->catelogues),
+            'colors'         => BrandModelColorResource::collection($this->colors),
+            'specifications' => BrandModelSpicificationResource::collection($this->specifications),
+            'products'       => $this->when($request->routeIs('model.products'), ProductResource::collection($this->products)),
+
         ];
     }
 }
