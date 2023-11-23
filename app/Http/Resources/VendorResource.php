e0ed2b17403ff28181c8edf5d789478fff8c94d6
ihossain5 <ismail.hossain5169@gmail.com>
2023-11-23 11:42:37 +0600

    issue fixed

--- app/Http/Resources/VendorResource.php
+++ app/Http/Resources/VendorResource.php
@@ -17,6 +17,7 @@ class VendorResource extends JsonResource
         return [
             'access_token' => $this->when($request->routeIs('vendor.profile') == false, $this->token),
             'token_type' => $this->when($request->routeIs('vendor.profile') == false, 'bearer'),
+            'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
             'photo' => $this->photo,
